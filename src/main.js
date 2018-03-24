314c4792edbe85a580fb43df27d4ee337cd823aa
MuMeiNo <mumeino.iikigen@gmail.com>
2018-03-24 14:35:02 +0700

    added Google Analytics

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import store from "./store/";
 import App from "./App";
 import router from "./router";
 import 'semantic-ui-css/semantic.min.css';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
 
@@ -19,4 +20,12 @@ new Vue({
   router,
   store,
   render: h => h(App)
+});
+
+Vue.use(VueAnalytics, {
+  id: 'UA-116291575-1',
+  router,
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production'
+  }
 });
